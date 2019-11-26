5d163592f7e5b2b3b4d3d2ed2aac00bbc0ed17c4
Robin Andrea <robinandrea_2@yahoo.com>
2019-11-26 09:08:48 -0800

    completed assignment 1, albeit without solving the argument issue.  With recursion, you must either omit the initial number of the sequence, 0, or ignore it and display argument + 1 elements

--- assignment1.rb
+++ assignment1.rb
@@ -6,3 +6,13 @@ def fibs(number)
 
     return fib
 end
+
+def fibs_rec(n, fibs = [])
+  return fibs << 0 if n == 0
+
+  fibs_rec(n - 1, fibs)
+
+  return fibs << 1 if n == 1 || n == 2
+  
+  fibs << (fibs[-1] + fibs[-2])
+end
